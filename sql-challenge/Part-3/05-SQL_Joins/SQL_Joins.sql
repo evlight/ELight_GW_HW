25e0c6189f4bad888f2f6f6ec931d54ad5d6b80b
evlight <erickalight1@gmail.com>
2019-11-19 23:04:29 -0500

    part3

--- sql-challenge/Part-3/05-SQL_Joins/SQL_Joins.sql
+++ sql-challenge/Part-3/05-SQL_Joins/SQL_Joins.sql
@@ -1,2 +1,10 @@
-SELECT * FROM first_table, second_table;
+----------------------PART 1--------------------------------------------
+--there are 4 different types of JOIN clauses in SQL: 
+--INNER, LEFT, RIGHT,  and FULL.
+-- As a special case, there may be a CROSS JOIN, which is a table 
+--(base table, view, or joined table) can JOIN to itself in a self-join. 
+--A programmer declares a JOIN statement to identify rows for joining.
+----------------------PART 2--------------------------------------------
+
+----The JOIN used to create the final view was a left join. 
 
